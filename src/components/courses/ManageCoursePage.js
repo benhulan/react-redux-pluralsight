f1ea783f0d84fbe87111645f8e10456a00b12413
Ben Hulan <ben.hulan@bounteous.com>
2020-03-13 10:08:15 -0500

    Add client-side validation for ManageCourse form

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -17,7 +17,7 @@ function ManageCoursePage({
   history,
   ...props
 }) {
-  const [course, setCourse] = useState({ ...props.course });
+  const [course, setCourse] = useState({ ...(props.course || {}) });
   const [errors, setErrors] = useState({});
   const [saving, setSaving] = useState(false);
 
@@ -45,8 +45,22 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if errors object has no properties
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
     saveCourse(course)
       .then(() => {
@@ -75,7 +89,7 @@ function ManageCoursePage({
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
